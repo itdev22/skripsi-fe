c0230cc780f18721886e9d92328fc10830d57bc7
Rama <90917112+itdev22@users.noreply.github.com>
2025-04-15 10:45:01 +0700

    Update FormAddComponent.vue

--- pages/dashboard/customer/FormAddComponent.vue
+++ pages/dashboard/customer/FormAddComponent.vue
@@ -13,12 +13,15 @@ const schema = object({
     phone: string()
         .min(3, 'Must be at least 3 characters')
         .required('Required'),
-    address: string()
-        .min(3, 'Must be at least 3 characters')
-        .required('Required'),
     packet_internet: string()
         .min(3, 'Must be at least 3 characters')
-        .required('Required')
+        .required('Required'),
+    ip_static: string()
+        .min(3, 'Must be at least 3 characters')
+        .required('Required'),
+    mac_address: string()
+        .min(3, 'Must be at least 3 characters')
+        .required('Required'),
 })
 
 type Schema = InferType<typeof schema>
@@ -32,7 +35,9 @@ const state = reactive({
     area_code: '',
     latitude: 0,
     longitude: 0,
-    packet_internet: undefined
+    packet_internet: undefined,
+    ip_static: '',
+    mac_address: '',
 })
 
 async function onSubmit(event: FormSubmitEvent<Schema>) {
@@ -72,6 +77,13 @@ async function moveToMyLocation() {
         console.error('Geolocation is not supported by this browser.')
     }
 }
+
+watch(() => state.ip_static, (newIp) => {
+    state.mac_address = newIp
+    console.log('IP Static changed:', newIp)
+},
+)
+
 </script>
 
 <template>
@@ -88,9 +100,7 @@ async function moveToMyLocation() {
         <UFormGroup label="Phone" name="phone">
             <UInput v-model="state.phone" />
         </UFormGroup>
-        <UFormGroup label="Address" name="address">
-            <UInput v-model="state.address" />
-        </UFormGroup>
+
         <UFormGroup label="Area Code" name="area_code">
             <UInput v-model="state.area_code" />
         </UFormGroup>
@@ -102,6 +112,9 @@ async function moveToMyLocation() {
                 <UButton @click="moveToMyLocation">My Position</UButton>
             </LControl>
         </LMap>
+        <UFormGroup label="Address" name="address">
+            <UInput v-model="state.address" />
+        </UFormGroup>
         <UFormGroup label="Map" name="coordinates">
             <div class="flex justify-around ">
                 <UInput v-model="state.latitude" class="w-full" placeholder="Latitude" type="number" />
@@ -110,6 +123,12 @@ async function moveToMyLocation() {
         </UFormGroup>
         <UFormGroup label="Packet Internet" name="packet_internet">
             <UInput v-model="state.packet_internet" />
+        </UFormGroup>
+        <UFormGroup label="Ip Static" name="ip_static">
+            <UInput v-model="state.ip_static" />
+        </UFormGroup>
+        <UFormGroup label="Mac Address" name="mac_address">
+            <UInput v-model="state.mac_address" />
         </UFormGroup>
 
         <UButton type="submit">
