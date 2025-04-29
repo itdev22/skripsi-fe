4530e7a49503831f0b55747d23f3760cfcc7b931
Rama <90917112+itdev22@users.noreply.github.com>
2025-04-29 15:10:05 +0700

    Update index.vue

--- pages/dashboard/customer/index.vue
+++ pages/dashboard/customer/index.vue
@@ -108,8 +108,8 @@ const items = (row: Person) => [
         click: () => window.open(row.gmaps_link, '_blank')
     }, {
         label: 'Upgrade',
-        icon: 'i-heroicons-arrow-right-circle-20-solid',
-        click: () => window.open(row.gmaps_link, '_blank')
+        icon: 'i-heroicons-arrow-up-circle-20-solid',
+        click: () => window.open("", '_blank')
     }], [{
         label: 'Delete',
         icon: 'i-heroicons-trash-20-solid'
