3978658bcf184549cb1fae587b3dd89bd6c91949
Rama <90917112+itdev22@users.noreply.github.com>
2025-05-05 10:47:53 +0700

    slicing page user management (#5)
    Co-authored-by: angga-sa <angga.s@kalachakra.io>

--- pages/dashboard/user-management/index.vue
+++ pages/dashboard/user-management/index.vue
@@ -1,113 +1,40 @@
 <script setup lang="ts">
-import FormAddComponent from './FormAddComponent.vue'
-const people = [{
-    id: 1,
-    name: 'John Doe',
-}]
+import Role from './role/Role.vue';
+import User from './user/User.vue';
 
-type Person = {
-    id: number
-    name: string
-}
 
-const columns = [
-    {
-        key: 'id',
-        label: 'Number'
-    }, {
-        key: 'name',
-        label: 'Name'
-    },{
-        key: 'actions',
-        label: 'Actions'
-    }
-]
 
-const page = ref(1)
-const pageCount = 5
-
-const rows = computed(() => {
-    return people.slice((page.value - 1) * pageCount, (page.value) * pageCount)
-})
-
-const q = ref('')
-
-let peopleData = people
-
-const filteredRows = computed(() => {
-    if (!q.value) {
-        peopleData = people
-        return people.slice((page.value - 1) * pageCount, (page.value) * pageCount)
-    }
-
-    const newData = people.filter((person) => {
-        return Object.values(person).some((value) => {
-            // person with paginate
-            return String(value).toLowerCase().includes(q.value.toLowerCase())
-        })
-    })
-    peopleData = newData
-    return newData.slice((page.value - 1) * pageCount, (page.value) * pageCount)
-})
-
-const isOpen = ref(false)
-
-const items = (row: Person) => [
-    [{
-        label: 'Edit',
-        icon: 'i-heroicons-pencil-square-20-solid',
-        click: () => console.log('Edit', row.id)
-    }], [{
-        label: 'Delete',
-        icon: 'i-heroicons-trash-20-solid'
-    }]
-]
-
-const toast = useToast()
-const modal = useModal()
-const count = ref(0)
-
-// function OpenModalReportInstallation(row: { id: number }) {
-//     modal.open(FormCustomerInstallation, {
-//         count: count.value,
-//         onSuccess() {
-//             toast.add({
-//                 title: 'Success !',
-//                 id: 'modal-success'
-//             })
-//         }
-//     })
-// }
-
+const tab_items = [
+  {
+    label: "User Management",
+    value: "user",
+  },
+  {
+    label: "Role",
+    value: "role",
+  },
+  // {
+  //   label: "Report Customer Installation",
+  //   value: "report-customer",
+  // },
+];
 </script>
 
 <template>
-    <UButton label="Add New User Access" @click="isOpen = true" />
-    <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
-        <UInput v-model="q" placeholder="Filter people..." />
-    </div>
+  <UTabs :items="tab_items" class="w-full">
+    <template #item="{ item }">
+      <div v-if="item.value == 'user'">
+        <User />
+      </div>
 
-    <UTable :rows="filteredRows" :columns="columns">
+      <div v-if="item.value == 'role'">
+        <Role />
+      </div>
 
-        <template #actions-data="{ row }">
-            <UDropdown :items="items(row)">
-                <UButton color="gray" variant="ghost" icon="i-heroicons-ellipsis-horizontal-20-solid" />
-            </UDropdown>
-        </template>
-    </UTable>
+      <!-- <div v-if="item.value == 'report-customer'">
+        <CustomerInstallation />
 
-    <div class="flex justify-end px-3 py-3.5 border-t border-gray-200 dark:border-gray-700">
-        <UPagination v-model="page" :page-count="pageCount" :total="peopleData.length" />
-    </div>
-    
-    <div>
-
-        <UModal v-model="isOpen">
-            <div class="p-4">
-                <Placeholder class="h-48" />
-                <FormAddComponent></FormAddComponent>
-
-            </div>
-        </UModal>
-    </div>
+      </div> -->
+    </template>
+  </UTabs>
 </template>
